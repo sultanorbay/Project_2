e496ac31d35119c838e2a93f6deed6486b91d27e
w10 <glsmh.16@gmail.com>
2023-05-25 23:23:36 +0300

    third commit

--- src/test/java/com/cydeo/conflict/Exercise.java
+++ src/test/java/com/cydeo/conflict/Exercise.java
@@ -2,6 +2,6 @@ package com.cydeo.conflict;
 
 public class Exercise {
     public static void main(String[] args) {
-        System.out.println("hello"+"world");
+        System.out.println("hello"+"cydeo");
     }
 }
